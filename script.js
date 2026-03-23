ad79defb0f27ca63f20ad3e6328d7dd4b0e946b1
Rami Essamedeen <rami.e@tsti.ae>
2026-03-23 13:11:37 +0400

    added updated after constraint

--- script.js
+++ script.js
@@ -3,18 +3,19 @@ const dotenv = require("dotenv");
 const { MongoClient } = require("mongodb");
 
 dotenv.config();
-debugger
+
 const MONGO_URI = process.env.MONGO_URI;
 const client = new MongoClient(MONGO_URI);
 
 const config = [{
   APP_NAME: "TSTI",
-  BASE_URL: "https://tsti.ae/version-test/api/1.1/obj/",
+  BASE_URL: "https://tsti.ae/version-test",
   TABLES: ["Classes", "Trainees", "New Request"]
 }]
 
 config.forEach((conf) => {
   conf.API_KEY = process.env[conf.APP_NAME + "_API_KEY"];
+  conf.BASE_URL = `${conf.BASE_URL}/api/1.1/obj/`
 });
 
 
@@ -23,19 +24,19 @@ const API_KEY = process.env.API_KEY;
 // Optional: max items per request (Bubble default is 100)
 const LIMIT = 100;
 
-async function fetchAllData(config, lastModifiedDate) {
+async function fetchAllData(config) {
   await client.connect();
+  const currentDatetime = new Date()
   const db = client.db(config.APP_NAME);
   const metadataCollection = db.collection("metadata");
   for (const table of config.TABLES) {
-    const doc = await metadataCollection.findOne({ table })
+    const tableMetadata = await metadataCollection.findOne({ table })
 
 
-    if (doc === null) {
+    if (tableMetadata === null) {
       await metadataCollection.insertOne({ table, lastModifiedDate: new Date(0).toISOString() });
-    } else if (new Date(doc.lastModifiedDate) >= lastModifiedDate) {
-      continue
     }
+
     const collection = db.collection(table);
     let cursor = 0;
     let totalFetched = 0;
@@ -53,7 +54,7 @@ async function fetchAllData(config, lastModifiedDate) {
               {
                 key: "Modified Date",   // your field
                 constraint_type: "greater than",   // greater than
-                value: lastModifiedDate.toISOString() // e.g. "2026-03-23T00:00:00Z"
+                value: tableMetadata ? new Date(new Date(tableMetadata.lastModifiedDate).getTime() - 60000).toISOString() : new Date(0).toISOString() // e.g. "2026-03-23T00:00:00Z"
               }
             ])
           },
@@ -62,7 +63,7 @@ async function fetchAllData(config, lastModifiedDate) {
         const data = response.data;
 
         await data.response.results.forEach(async (item) => {
-          await collection.updateOne({ _id: item._id }, { $set: { [new Date()]: item } }, { upsert: true });
+          await collection.updateOne({ _id: item._id }, { $push: { snapshots: { ...item, savedAt: currentDatetime.toISOString() } } }, { upsert: true });
         });
 
         // Update cursor
@@ -73,8 +74,8 @@ async function fetchAllData(config, lastModifiedDate) {
         totalFetched += data.response.results.length;
 
         if (!hasMore) {
-          console.log(`All data fetched! Total records: ${totalFetched}`);
-          await metadataCollection.updateOne({ table }, { $set: { lastModifiedDate: lastModifiedDate.toISOString() } });
+          console.log(`[${config.APP_NAME} - ${table}] All data fetched! Total records: ${totalFetched}`);
+          await metadataCollection.updateOne({ table }, { $set: { lastModifiedDate: currentDatetime.toISOString() } });
         }
       }
 
@@ -86,7 +87,10 @@ async function fetchAllData(config, lastModifiedDate) {
 }
 
 // Run the script
-fetchAllData(config[0], new Date("2026-01-24T00:00:00Z"))
+
+fetchAllData(config[0])
   .catch((err) => {
     console.error("Failed:", err);
+  }).finally(() => {
+    client.close();
   });
